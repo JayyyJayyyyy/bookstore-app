3b95f0000722f064ce6265f4f4d5f800a9b95625
JayyyJayyyyy <150527316+JayyyJayyyyy@users.noreply.github.com>
2024-04-07 02:37:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 locals {
   instance-type = "t2.micro"
-  key-name = "clarusway"
+  key-name = "firstkey"
   secgr-dynamic-ports = [22,80,443,8080,5000,3306]
   user = "clarusway"
 }
