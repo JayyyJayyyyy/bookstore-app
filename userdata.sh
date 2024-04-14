f4c20df3ec86634be6656f10a85c1eca40247569
JayyyJayyyyy <150527316+JayyyJayyyyy@users.noreply.github.com>
2024-04-14 21:56:19 +0300

    Create userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,17 @@
+              #!/bin/bash
+              hostnamectl set-hostname docker_instance
+              yum update -y
+              yum install git -y
+              yum install docker -y
+              systemctl start docker
+              systemctl enable docker
+              TOKEN=${user-data-git-token}
+              USER=${user-data-git-name}
+              usermod -a -G docker ec2-user
+              newgrp docker
+              #install docker-compose
+              curl -SL https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+              chmod +x /usr/local/bin/docker-compose
+              cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/bookstore-app.git
+              cd bookstore-app
+              docker-compose up
